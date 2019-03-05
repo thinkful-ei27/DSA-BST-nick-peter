1221af848cfd0112c7f46d77b1f188b1b4204393
nicknotj <nicknotjustin@gmail.com>
2019-03-05 11:44:24 -0600

    Nick does not like github when github does not recognize that Nick exists. Also, added a find method to the Binary Search Tree.

--- BST.js
+++ BST.js
@@ -8,6 +8,9 @@ class BinarySearchTree {
   }
 
   insert(key, value) {
+    //BigO notation: Average case: O(log(n)), worse case (unbalanced tree): O(n)
+    //Best case: O(1), empty tree
+
     // if the tree is empty then this key become the root of the tree
     if (this.key === null) {
       this.key = key;
@@ -22,7 +25,32 @@ class BinarySearchTree {
       // if there is then we recursilely call the insert function 
       if (this.left === null) {
         this.left = new BinarySearchTree(key, value, this);
-      } 
+      } else {
+        return this.left.insert(key, value);
+      }
+    } else {
+      //key > this.key
+      if(this.right === null){
+        this.right = new BinarySearchTree(key, value, this);
+      } else {
+        return this.right.insert(key, value);
+      }
     }
-  }  
+  }
+  
+  find(key){
+    //BigO notation: Best: O(1) root key === key Average: O(log(n)) don't have to search
+    //all the data Worst: O(n), very unbalanced tree 
+    //check if this.key === key
+    if(this.key === key){
+      return this.value;
+    } else if(key < this.key && this.left){
+        return this.left.find(key);
+      } 
+      else if (key > this.key && this.right){
+        return this.right.find(key);
+      } else {
+        return 'The key does not exist within the Tree.';
+      }
+  }
 }
